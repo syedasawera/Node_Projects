763c5db0061dd24594d81dc1f0f4c1c4fa3ec382
Sawera <142902770+syedasawera@users.noreply.github.com>
2023-11-10 14:38:38 +0500

    Add files via upload

---
+++ WordCounter/main.ts
@@ -0,0 +1,37 @@
+import { differenceInSeconds } from "date-fns"
+import { el } from "date-fns/locale";
+import inquirer from "inquirer"
+
+const res = await inquirer.prompt({
+    type: "number",
+    name: "userInput",
+    message: "Please enter the number of seconds for the countdown:",
+    validate: (input) => {
+        if (isNaN(input)) {
+            return "Please enter a valid number"
+        } else if (input <= 0) {
+            return "Seconds must be greater than 0"
+        } else {
+            return true;
+        }
+    }
+});
+
+let input = res.userInput;
+
+function startCountdown(seconds : any) {
+    const interval = setInterval(() => {
+        console.clear(); // Clear the console to update the countdown
+        console.log(`Countdown: ${seconds} seconds`);
+
+        if (seconds === 0) {
+            clearInterval(interval);
+            console.log("Countdown has ended!");
+            process.exit();
+        }
+
+        seconds--;
+    }, 1000);
+}
+
+startCountdown(input);
